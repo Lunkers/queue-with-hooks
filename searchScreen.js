e1d3c5b1bbf1b52729bf3ef4d40f77ade2bca18a
Fredrik Lundkvist <flundkvi@kth.se>
2019-10-17 13:00:53 +0200

    data

--- searchScreen.js
+++ searchScreen.js
@@ -17,12 +17,12 @@ const SearchView = ({ navigation }) => {
     const [filter, updateFilter] = React.useState(null)
 
     const searchResults = [
-        { title: 'I love my wife', service: 'Spotify', image: require('./images/album1.png'), mediaId: "Song1", youtubeId: "TQcGnEhciNY" },
+        { title: 'I love my wife', service: 'Spotify', image: require('./images/chance.png'), mediaId: "Song1", youtubeId: "TQcGnEhciNY" },
         { title: 'Friends', service: 'Netflix', image: require('./images/fronds.png'), mediaId: 'Netflix1', youtubeId: "Kl7WUJRyyEk" },
-        { title: 'Doot Doot xd', service: 'Youtube', image: require('./images/video1.png'), mediaId: 'Youtube1', youtubeId: "eVrYbKBrI7o" },
-        { title: 'Jackbox', service: 'Steam', image: require('./images/album2.png'), mediaId: 'jackbox', youtubeId: "yhW8PfKB828" },
-        { title: 'overcooked', service: 'Steam', image: require('./images/album2.png'), mediaId: 'overcooked', youtubeId: "0n1x-zd7gZM" },
-        { title: 'music 3', service: 'Spotify', image: require('./images/album2.png'), mediaId: 'Song3', youtubeId: "YU3eDa8ehzc" },
+        { title: 'Vine compilation', service: 'Youtube', image: require('./images/youtube.jpg'), mediaId: 'Youtube1', youtubeId: "eVrYbKBrI7o" },
+        { title: 'Jackbox', service: 'Steam', image: require('./images/steam1.jpg'), mediaId: 'jackbox', youtubeId: "yhW8PfKB828" },
+        { title: 'overcooked', service: 'Steam', image: require('./images/steam2.png'), mediaId: 'overcooked', youtubeId: "0n1x-zd7gZM" },
+        { title: 'Peppa Pig', service: 'Netflix', image: require('./images/netflix1.png'), mediaId: 'Song3', youtubeId: "YU3eDa8ehzc" },
     ]
 
 
@@ -55,7 +55,7 @@ const SearchView = ({ navigation }) => {
     return (
         <SafeAreaView style={styles.scrollView}>
             <View style={styles.searchBox}>
-                <MaterialIcons name="search" size={32} color="#A64253" />
+                <MaterialIcons name="search" size={32} color="white" />
                 <TextInput placeholder="search" onChange={text => updateSearchText(text)} style={{ width: "75%" }} />
                 <Button title="filters" color="#FCE7CF" borderRadius={5} onPress={() => changeShowFilters(!showFilters)}></Button>
             </View>
